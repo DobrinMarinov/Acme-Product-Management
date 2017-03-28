3b59b34dcf22ced04926916c0d0d5e4d89f2290a
DESKTOP-LI4L696\Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-03-28 10:27:21 +0300

    Cross-origin resource sharing configured. Accesing data bug fixed

--- APM.WebApi/App_Start/WebApiConfig.cs
+++ APM.WebApi/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -19,6 +20,13 @@ namespace APM.WebApi
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //config.EnableCors();
+
+            var corsAttr = new EnableCorsAttribute("http://localhost:51639", "*", "*");
+            config.EnableCors(corsAttr);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
---
+++ APM.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using APM.WebApi.Models;
+using APM.WebAPI.Models;
+
+namespace APM.WebApi.Controllers
+{
+    //[EnableCors(origins: "http://localhost:51639", headers: "*", methods:"*")]
+    public class ProductsController : ApiController
+    {
+        // GET: api/Product
+        public IEnumerable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.Retrieve();
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
